befae3b4aa7816f0c8a08987c37053966acc7bf0
nehamundye <nehamundye@gmail.com>
2021-06-13 21:25:05 +0900

    made layout responsive and other small css changes

--- quiz-app/quiz/mathquiz/static/mathquiz/index.js
+++ quiz-app/quiz/mathquiz/static/mathquiz/index.js
@@ -1,121 +1,77 @@
+const questionId = document.querySelector(".question").dataset.id;
+const options = document.querySelectorAll(".option");
+const message = document.querySelector("#message");
+const nextBtn = document.querySelector("#nextBtn");
+const currentscore = document.querySelector("#currentscore");
+const currentprogressbar = document.querySelector(".currentprogressbar");
 
-const questionId = document.querySelector('.question').dataset.id;
-// console.log(questionId)
-const options = document.querySelectorAll('.option')
-const message = document.querySelector('#message')
-const nextBtn = document.querySelector('#nextBtn')
-const currentscore = document.querySelector('#currentscore')
-const currentprogressbar = document.querySelector('.currentprogressbar')
-
-// localStorage.setItem('score', 0)
-// let score = 0
 // If no score in localStorage, set it to 0
-if (!localStorage.getItem('score')) {
-    localStorage.setItem('score', 0);
+if (!localStorage.getItem("score")) {
+  localStorage.setItem("score", 0);
 }
 
+options.forEach((option) => {
+  option.addEventListener("click", () => {
+    option_number = option.dataset.id;
+    nextBtn.style.display = "inline";
+    let score = localStorage.getItem("score");
 
+    // check if for questionId, if option selected is correct or not.
+    // If option selected is correct, then change option background color to green
+    fetch(`/${questionId}/${option_number}`)
+      .then((response) => response.json())
+      .then((answer) => {
+        if (answer.isCorrect === "true") {
+          option.style.backgroundColor = "green";
+          message.innerHTML = "Well Done! &#128512;";
+          message.style.color = "green";
 
-options.forEach(option => {
-    option.addEventListener('click', () => {
-        // console.log(option.dataset.id)
-        // console.log(`questionId: ${questionIdn}`)
+          score++;
+          localStorage.setItem("score", score);
+          currentscore.innerHTML = `${score}`;
 
-        option_number = option.dataset.id
-        // console.log(`optionId: ${option}`)
+          options.forEach((option) => {
+            option.classList.add("disableddivs");
+          });
 
-        // nextBtn.classList.add('show-btn');
-        // console.log(nextBtn)
-        nextBtn.style.display = 'inline'
+          // If option selected is incorrect
+        } else {
+          // If option selected is incorrect, then change option background color to red
+          option.style.backgroundColor = "red";
+          option.style.backgroundColor = "red";
+          message.innerHTML = "Opps, the answer is incorrect. &#128577;";
+          message.style.color = "red";
 
+          console.log(`Score is: ${score}`);
+          currentscore.innerHTML = `${score}`;
 
-        // currentprogressbar.setAttribute('id', 'play-animation')
-        
+          // Disable all options/divs
+          options.forEach((option) => {
+            option.classList.add("disableddivs");
+          });
 
-        let score = localStorage.getItem('score');
-
-        // check if for questionId, if option selected is correct or not.
-        // If option selected is correct, then change option background color to green
-        
-        fetch(`/${questionId}/${option_number}`)
-        .then(response => response.json())
-        .then(answer => {
-            // console.log(answer.isCorrect)
-            // console.log(`correctAnswer is: ${answer.correctAnswer}`)
-
-            if (answer.isCorrect === 'true') {
-                // console.log("correct")
-                option.style.backgroundColor = 'green'
-                message.innerHTML = "Well Done! &#128512;"
-                message.style.color = 'green'
-
-                
-
-                score++;
-                localStorage.setItem('score', score);
-                // console.log(`Score is: ${score}`)
-                currentscore.innerHTML = `${score}`
-
-                
-
-                // console.log(options)
-
-                options.forEach(option => {
-                    option.classList.add('disableddivs');
-                })
-
-            // If option selected is incorrect
-            } else {
-                // console.log("incorrect")
-                // If option selected is incorrect, then change option background color to red
-                option.style.backgroundColor = 'red'
-                option.style.backgroundColor = 'red'
-                message.innerHTML = "Opps, the answer is incorrect. &#128577;"
-                message.style.color = 'red'
-
-                console.log(`Score is: ${score}`)
-                currentscore.innerHTML = `${score}`
-
-                // Disable all options/divs
-                options.forEach(option => {
-                    option.classList.add('disableddivs');
-                })
-
-                // Show the correct answer in green background
-                options.forEach(option => {
-                    if (option.dataset.id === answer.correctAnswer) {
-                        option.style.backgroundColor = 'green'
-                    }
-                })
+          // Show the correct answer in green background
+          options.forEach((option) => {
+            if (option.dataset.id === answer.correctAnswer) {
+              option.style.backgroundColor = "green";
             }
-
-        })
-    })
+          });
+        }
+      });
+  });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  if (document.querySelector("#message").innerHTML === "Quiz Over") {
+    let score = localStorage.getItem("score");
+    const div = document.createElement("div");
+    div.className = "finalscore";
+    message.append(div);
+    div.innerHTML = `Your Final Score: ${score}`;
 
+    localStorage.setItem("score", 0);
+  }
+});
 
-
-document.addEventListener('DOMContentLoaded', function() {
-    if (document.querySelector('#message').innerHTML === 'Quiz Over') {
-
-        let score = localStorage.getItem('score')
-        // console.log(`End score: ${score}`)
-
-        const div = document.createElement('div');
-        div.className = 'finalscore';
-        message.append(div)
-        div.innerHTML = `Your Final Score: ${score}`
-
-        localStorage.setItem('score', 0)
-
-    }
-})
-
-
-let score = localStorage.getItem('score');
-currentscore.innerHTML = `${score}`
-
-
-
-
+let score = localStorage.getItem("score");
+currentscore.innerHTML = `${score}`;
--- quiz-app/quiz/mathquiz/views.py
+++ quiz-app/quiz/mathquiz/views.py
@@ -2,32 +2,19 @@ from django.shortcuts import HttpResponse, HttpResponseRedirect, render
 from django.urls import reverse
 import json
 from django.http import JsonResponse
-
 import random
-
 from .models import Quiz
 from django.db.models import Avg, Max, Min, Sum
 
-# Create your views here.
+
 def index(request):
-    # request.session["quizids"] = []
-
-    list_of_quizids = Quiz.objects.order_by('id').values_list('id', flat=True).distinct()
+    list_of_quizids = Quiz.objects.order_by(
+        'id').values_list('id', flat=True).distinct()
     total_questions = Quiz.objects.all().count()
-    # min_quiz_id = Quiz.objects.all().aggregate(Min('id')).get('id__min')
-    # max_quiz_id = Quiz.objects.all().aggregate(Max('id')).get('id__max')
-    # random_number = random.randint(min_quiz_id, max_quiz_id)
-    # quiz = Quiz.objects.get(id=random_number)
 
-
-
-    # print(request.session)
     if "quizids" not in request.session:
-        # print("entered request session")
         request.session["quizids"] = []
-        # print(request.session["quizids"])
         first_random_number = random.choice(list_of_quizids)
-        # print("first_random_number is:", first_random_number)
         quiz = Quiz.objects.get(id=first_random_number)
         request.session["quizids"] += [first_random_number]
         print(request.session["quizids"])
@@ -37,7 +24,6 @@ def index(request):
             "question_num": question_num
         })
     else:
-        
         print("entered else clause")
         # remove used quizids
         list_to_remove = request.session["quizids"]
@@ -50,28 +36,24 @@ def index(request):
 
         if not len(request.session["quizids"]) > 10:
             quiz = Quiz.objects.get(id=random_number)
-            print("length: ",len(request.session["quizids"]) )
-            if len(request.session["quizids"]) == 10 :
+            print("length: ", len(request.session["quizids"]))
+            if len(request.session["quizids"]) == 10:
                 return render(request, "mathquiz/index.html", {
                     "quiz": quiz,
                     "endButton": "endButton",
                     "question_num": question_num
                 })
-            
             return render(request, "mathquiz/index.html", {
-                    "quiz": quiz,
-                    "question_num": question_num
-                })
+                "quiz": quiz,
+                "question_num": question_num
+            })
         else:
             return render(request, "mathquiz/endquiz.html", {
                 "quiz": "placeholder"
             })
-        
+
 
 def checkanswer(request, questionId, option_number):
-    # print(questionId)
-
-    # If option of questionId is equal to answer, 
     quiz = Quiz.objects.get(id=questionId)
     print(quiz.answer)
     print(option_number)
@@ -79,6 +61,7 @@ def checkanswer(request, questionId, option_number):
         return JsonResponse({"isCorrect": "true", "correctAnswer": quiz.answer})
     else:
         return JsonResponse({"isCorrect": "false", "correctAnswer": quiz.answer})
+
 
 def replay(request):
     request.session["quizids"] = []
