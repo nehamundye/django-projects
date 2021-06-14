1d03b645233eab0cab46f4f38aea12aba3278f9b
nehamundye <nehamundye@gmail.com>
2021-06-14 18:51:14 +0900

    formatted code, removed print statements

--- quiz-app/quiz/mathquiz/views.py
+++ quiz-app/quiz/mathquiz/views.py
@@ -8,8 +8,7 @@ from django.db.models import Avg, Max, Min, Sum
 
 
 def index(request):
-    list_of_quizids = Quiz.objects.order_by(
-        'id').values_list('id', flat=True).distinct()
+    list_of_quizids = Quiz.objects.order_by('id').values_list('id', flat=True).distinct()
     total_questions = Quiz.objects.all().count()
 
     if "quizids" not in request.session:
@@ -17,26 +16,21 @@ def index(request):
         first_random_number = random.choice(list_of_quizids)
         quiz = Quiz.objects.get(id=first_random_number)
         request.session["quizids"] += [first_random_number]
-        print(request.session["quizids"])
         question_num = len(request.session["quizids"])
         return render(request, "mathquiz/index.html", {
             "quiz": quiz,
             "question_num": question_num
         })
     else:
-        print("entered else clause")
         # remove used quizids
         list_to_remove = request.session["quizids"]
         available_quiz_ids = list(set(list_of_quizids) - set(list_to_remove))
-        print("available_quiz_ids: ", available_quiz_ids)
         random_number = random.choice(available_quiz_ids)
         request.session["quizids"] += [random_number]
-        print("request.session quizids", request.session["quizids"])
         question_num = len(request.session["quizids"])
 
         if not len(request.session["quizids"]) > 10:
             quiz = Quiz.objects.get(id=random_number)
-            print("length: ", len(request.session["quizids"]))
             if len(request.session["quizids"]) == 10:
                 return render(request, "mathquiz/index.html", {
                     "quiz": quiz,
@@ -55,8 +49,6 @@ def index(request):
 
 def checkanswer(request, questionId, option_number):
     quiz = Quiz.objects.get(id=questionId)
-    print(quiz.answer)
-    print(option_number)
     if quiz.answer == option_number:
         return JsonResponse({"isCorrect": "true", "correctAnswer": quiz.answer})
     else:
